096c31a104a426e5c7d8bfc49429f9ca679c8e42
khaled-hbaieb <khaledhbaieb@yahoo.fr>
2020-03-25 15:16:33 +0100

    working on meet and greet

--- prepExercises.js
+++ prepExercises.js
@@ -51,11 +51,16 @@ return result
 // More Practice
 // 1.Go back through any of the exercises done in the introduction to repetition and refactor them to use the while loop.
 
-
+// Already Done.
 // 2.Fencepost Loop: Use a while loop to build a single string with the numbers 1 through n, separated by commas. Have it return the new string. How can we make sure not to have a comma after the last number?
-//  function counting(n) { 
-//        // TODO: your code here 
-//  } 
+function counting (n) {
+var result=n
+while (n>1){
+	 n=n-1
+	 result=n+','+result
+}
+return result
+} 
 //  counting(5); // => '1, 2, 3, 4, 5' 
 //  counting(1); // => '1' 
 //  counting(3); // => '1, 2, 3'
@@ -70,14 +75,46 @@ return result
 //  welcome 4, meet 1, 2 and 3 
 //  welcome 5, meet 1, 2, 3 and 4
 // HINT: search for which symbol that creates a line break in a string.
+function counting (n) {
+var result=n
+while (n>1){
+	 n=n-1
+	 result=n+','+result
+}
+return result
+} 
 
+function meetGreet(n){
 
+var result = 'welcome 1' +'\n' + 'welcome 2, meet 1' + '\n' + 'welcome 3, meet 1 and 2'
+if(n === 1){
+return 'welcome 1'
+}
+else if (n === 2){
+return 'welcome 1' +'\n' + 'welcome 2, meet 1'
+}
+else if(n === 3){
+return 'welcome 1' +'\n' + 'welcome 2, meet 1' + '\n' + 'welcome 3, meet 1 and 2'
+} else if (n>3) {
+for (var i =4; i< n; i++){
+
+result = result + '\n' + 'welcome ' + i + ' meet' + counting(i)
+}
+}
+return result
+}
 
 
 // 4.Reverse String: Write a function that takes a string as an input and returns the reverse of that string.
-//  function reverseString(str) { 
-//        // TODO: your code here 
-//  } 
+function reverseString(str) { 
+ var result = str[str.length - 1];
+ var i = str.length - 2
+ while(i >= 0){
+ 	result = result + str[i]
+i--
+ }
+ return result;
+} 
 //  reverseString('hello'); // => 'olleh' 
 //  reverseString('1#*$'); // => '$*#1'
 // Do this using a while loop.
@@ -86,6 +123,17 @@ return result
 
 // 5.Write a javascript program to print all Prime numbers from 1 to n.
 
+function primeNum (n) {
+	var result = '';
+	var i = 2
+	while(i < n){
+		if( n%i !== 0){
+			result = result + i
+		}
+		i++
+		}
+		return result
+	}
 
 
 
@@ -94,7 +142,9 @@ return result
 //  '0 is even' 
 //  '1 is odd' 
 //  '2 is even'
+function evenOrNot (n) {
 
+}
 
 
 
