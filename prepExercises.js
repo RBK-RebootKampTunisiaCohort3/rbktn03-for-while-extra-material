3b5238ba363bd0f21516e8c276261a74b7bdce60
ahmed-fenni <ahmedfenny8@gmail.com>
2020-03-25 16:24:59 +0100

    done

--- prepExercises.js
+++ prepExercises.js
@@ -99,7 +99,7 @@ var total =''
 		}
 		 else if(i>3){
 		 	total += 'welcome ' + i +', meet ' + counting(i-2) + ' and ' + (i-1) + '\n'
-		 }
+		}
     i++
 	}
   return total
@@ -134,21 +134,50 @@ var total =''
 
 
 // 5.Write a javascript program to print all Prime numbers from 1 to n.
+function PrimeNumbers(n){
+ 	var total = [1]
+ 	for(var i = 2;i<=n;i++){
+ 		if((i%2)!==0 && (i%3)!==0 && (i%5)!==0 && (i%7)!==0 && (i%11)!==0){
+ 			total.push(i)
+ 		
+ 	    }else if(i===2 || i===3 ||i===5||i===7||i===11 ){
+ 	    	total.push(i)
+ 	    }
 
-
-
+ 	}
+   return total
+ }
 
 // 6.Write a JavaScript for loop that will iterate from 0 to n. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
 // Sample Output : 
 //  '0 is even' 
 //  '1 is odd' 
 //  '2 is even'
-
-
+function EvenOrOdd(n){
+ var total = 0 + ' is even' 
+  var    i = 1  
+  while(i<=n){
+  	if ((i%2)!==0){
+  		total = i + ' is odd ' 
+  	}else if((i%2)===0){
+  		total = i + ' is even '
+  	}
+  i++
+}
+ return total
+  }
 
 
 // 7.Write a javascript program to enter any number and print all factors of the number.
-
+function factor(n){
+        var factors = [];
+        for (var i = 0; i <= n; i++){
+            if (n % i === 0){
+                factors.push(i);
+            }
+        }
+        return factors;
+}
 
 
 
@@ -158,7 +187,18 @@ var total =''
 //  firstAndLast(456) ==> 4 + 6 = 11 
 //  firstAndLast(123) ==> 1 + 3 = 4 
 //  firstAndLast(197) ==> 1 + 7 = 8
+function FirstAndLast(n){
+	var total = 0
+    var i = n
+    while(i>=0){
+    	if(i>=n){
+    	
+    		total+=Math.floor(n/100)
+           i = i - 1
+    	}
 
+    }
+}
 
 
 
