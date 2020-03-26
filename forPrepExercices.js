4f4f5810b821e1c6c3b04b9379f97be4cca9b5e8
khaled-hbaieb <khaledhbaieb@yahoo.fr>
2020-03-26 16:37:04 +0100

    reached extra material ex 4

--- forPrepExercices.js
+++ forPrepExercices.js
@@ -244,5 +244,13 @@ console.log(result)
 	return result.join('')
 }
 // 2.Write a function keep that 'keeps' certain elements in an array. The function will need to take two arguments, an array, and something else -- the second argument will be what is used to determine which elements to keep.
-
 // You should be able to use this function to write evens, evenLengthWords, a hypothetical odds function, or oddLengthWords without changing the keep function.
+function keep (arr, something) {
+	var result = [];
+	for (var i = 0; i < arr.length; i++) {
+		if (){
+			result.push(arr[i])
+		}
+	}
+	return result
+}
