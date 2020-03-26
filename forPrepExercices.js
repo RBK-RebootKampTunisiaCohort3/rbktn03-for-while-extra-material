de67d39291c2b4bbcb95dfb2a9511e3344065128
sami affes <sami_affes@yahoo.com>
2020-03-26 13:19:33 +0100

    solve

--- forPrepExercices.js
+++ forPrepExercices.js
@@ -55,22 +55,36 @@ function max(array) {
 //  Use split to write a function longestWord that takes a string as an argument and returns the longest word.
 
 function longestWord(str){
-	var array = str.split;
-	var result = str[0];
+	var array = str.split(' ');
+    var result = array[0];
 	var i = 1;
-	while (i < str.length){
-		if (str[i] > result){
-			result = str[i];
+	while (i < array.length){
+		if (array[i].length > result.length ){
+			result= array [i];
 		}
 			i++;
 	}
 			return result;
 
 }
+
+function longestWord(str){
+	var array = str.split(' ');
+	var result = array[0];
+		for(var i = 1; i < array.length ; i++){
+			if (array[i].length > result.length){
+					result = array[i];
+			}
+		}
+					return result;
+}
 // 4.Write a function remove that accepts an array and an element, and returns an array with all ocurrences of element removed.
 
 // function remove(array, element) {
 //  // your code here
+function remove(array,element){
+
+}
 //  }
 //  remove([1, 3, 6, 2, 3], 3); // => [1, 6, 2]
 // 5.Write a function evens that accepts an array as an argument, and returns an array consisting of all of the even numbers in that array.
